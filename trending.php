a1c6f368a511f117beb5d80f33030a37246deb29
Mack S <60047062+Time-Vault@users.noreply.github.com>
2023-04-06 20:09:26 -0700

    Trending sorts posts by upvotes

--- trending.php
+++ trending.php
@@ -25,85 +25,97 @@
 
         <div class="col-md-6 overflow-auto">
 
-          <div class="post">
-            <img src="res/img/p1.svg" alt="ppl" width="40" height="40" class="rounded-circle me-2" />
-            <div class="content">
-              <span class="post-title">Mom said my new cat is ugly [OC]</span>
-              <span class="post-text">tell her to apologize to your cat. right now.</span>
-            </div>
-            <div class="icon">→</div>
-          </div>
+          <?php
+          //connect to database
+          if ($error != null) {
+            $output = "<p>Unable to reach the database!</p>";
+            exit($output);
+          } else {
+            //display posts and which sublueddit they belong to
+            $stmt = $conn->prepare("SELECT p.pid, p.title, p.link, p.upvotes, p.content, p.sid, s.title AS stitle, u.username, u.profilepath
+            FROM posts p
+            INNER JOIN sublueddits s ON p.sid = s.sid
+            INNER JOIN users u ON p.userid = u.userid
+            ORDER BY p.upvotes DESC");
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $posts = [];
+            while ($row = $result->fetch_assoc()) {
+              array_push($posts, [
+                'pid' => $row['pid'],
+                'title' => $row['title'],
+                'link' => $row['link'],
+                'upvotes' => $row['upvotes'],
+                'content' => $row['content'],
+                'sid' => $row['sid'],
+                'stitle' => $row['stitle'],
+                'username' => $row['username'],
+                'profilepath' => $row['profilepath']
+              ]);
+            }
+            foreach ($posts as $post) { ?>
+              <div class="post">
 
-          <div class="post">
-            <img src="res/img/p2.svg" alt="ppl" width="40" height="40" class="rounded-circle me-2" />
-            <div class="content">
-              <span class="post-title">
-                He sleeps like the dead. I have an uncontrollable urge to give him rosary beads, just in case…
-              </span>
-              <span class="post-text">Meow father, who art in heaven…</span>
-            </div>
-            <div class="icon">→</div>
-          </div>
+                <img src="<?= $post['profilepath'] ?>" alt="ppl" width="40" height="40" class="rounded-circle me-2" />
+                <div class="content">
 
-          <div class="post">
-            <img src="res/img/p3.svg" alt="ppl" width="40" height="40" class="rounded-circle me-2" />
-            <div class="content">
-              <span class="post-title">Oh no, poor kitty ...</span>
-              <span class="post-text">
-                I don't think he has one orange brain cell. That's one smart orange tabby.
-              </span>
-            </div>
-            <div class="icon">→</div>
-          </div>
+                  <span class="post-title">
+                    <?= $post['title'] ?>
+                  </span>
 
-          <div class="post">
-            <img src="res/img/p4.svg" alt="ppl" width="40" height="40" class="rounded-circle me-2" />
-            <div class="content">
-              <span class="post-title">meet my new study buddy</span>
-              <span class="post-text">Gorgeous! What’s the name?</span>
-            </div>
-            <div class="icon">→</div>
-          </div>
+                  <span class="post-text">
+                    <?= $post['content'] ?>
+                  </span>
 
-          <div class="post">
-            <img src="res/img/p5.svg" alt="ppl" width="40" height="40" class="rounded-circle me-2" />
-            <div class="content">
-              <span class="post-title">
-                Never had a cat before this little guy. He stole my heart and my thoughts on cats.
-              </span>
-              <span class="post-text">Watch out. They’ll steal your chair too lol.</span>
-            </div>
-            <div class="icon">→</div>
-          </div>
+                  <?php
+                  // if link is a picture
+                  if ($post['link'] != null && (strpos($post['link'], '.jpg') !== false || strpos($post['link'], '.png') !== false)) {
+                    ?>
+                    <span class="post-link">
+                      <img src="<?= $post['link'] ?>" alt="post image" width="100%" height="auto" />
+                    </span>
+                  <?php } else if ($post['link'] != null) { ?>
+                      <span class="post-link">
+                        <a href="<?= $post['link'] ?>" class="text-muted">
+                          ⎋ External Link
+                        </a>
+                      </span>
+                  <?php } ?>
 
-          <div class="post">
-            <img src="res/img/p6.svg" alt="ppl" width="40" height="40" class="rounded-circle me-2" />
-            <div class="content">
-              <span class="post-title">She's so cute</span>
-              <span class="post-text">kittens have a way of capturing our hearts.</span>
-            </div>
-            <div class="icon">→</div>
-          </div>
 
-          <div class="post">
-            <img src="res/img/p7.svg" alt="ppl" width="40" height="40" class="rounded-circle me-2" />
-            <div class="content">
-              <span class="post-title">
-                She’s from the streets but I think she’s taking to couches and blankets quite well
-              </span>
-              <span class="post-text">They bites!</span>
-            </div>
-            <div class="icon">→</div>
-          </div>
+                  <div id="<?= $post['pid'] ?>" class="upvotes">
+                    <form method="post" action="upvotes.php">
+                      <input type="hidden" name="pid" value="<?= $post['pid'] ?>">
+                      <input type="hidden" name="upvoted" value="1">
+                      <input class="border-0 bg-info px-3 arrow text-light rounded-pill fw-bolder" type="submit" name="vote" value="&uparrow;" onclick="markArrowClickedUp(this)" />
+                    </form>
+                    <p>
+                      <?= $post['upvotes'] ?>
+                    </p>
+                    <form method="post" action="upvotes.php">
+                      <input type="hidden" name="pid" value="<?= $post['pid'] ?>">
+                      <input type="hidden" name="downvoted" value="1">
+                      <input class="border-0 bg-warning px-3 arrow text-light rounded-pill fw-bolder" type="submit" name="vote" value="&downarrow;" onclick="markArrowClickedDown(this)" />
+                    </form>
+                  </div>
 
-          <div class="post">
-            <img src="res/img/p8.svg" alt="ppl" width="40" height="40" class="rounded-circle me-2" />
-            <div class="content">
-              <span class="post-title">😻❄</span>
-              <span class="post-text">😻😻😻😻😻😻😻😻</span>
-            </div>
-            <div class="icon">→</div>
-          </div>
+                  <p>Posted by
+                    <?= $post['username'] ?>
+                    in
+                    <a class="text-muted" href="sublueddit.php?sid=<?= $post['sid'] ?>">
+                      b/<?= $post['stitle'] ?>
+                  </p>
+
+                  <?php if ($_SESSION['isAdmin'] == 1) { ?>
+                    <a style="float:right;" href="deletePost.php?pid=<?= $post['pid'] ?>">Delete Post</a>
+                  <?php } ?>
+
+                </div>
+                <div class="icon"><a class="text-dark" href="post.php?pid=<?= $post['pid'] ?>">→</a></div>
+              </div>
+            <?php }
+          }
+          ?>
         </div>
 
         <!-- Panel -->
