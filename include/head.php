d765c61bac4fc1c32f4fe35c2c6abf4933122e40
Roman <64708726+madromas@users.noreply.github.com>
2024-02-24 12:23:40 -0500

    Add files via upload

--- include/head.php
+++ include/head.php
@@ -109,10 +109,15 @@ if (isset($_SESSION['editError']) && $_SESSION['editError'] == true) {
 <!-- Bootstrap CSS -->
 <link href="res/bootstrap/bootstrap.min.css" rel="stylesheet" />
 <link rel="stylesheet" href="res/css/base.css" />
+<link rel="stylesheet" href="res/css/fancybox.css" />
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css" />
+
 
 <!-- Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+
+
 <script src="res/js/sidebars.js"></script>
+<script src="res/js/fancybox.js"></script>
